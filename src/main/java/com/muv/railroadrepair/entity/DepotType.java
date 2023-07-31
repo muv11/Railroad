ea81cb60629d66de3838befa459b62574e914069
muv11 <uvmozgovaya@proton.me>
2023-07-31 12:08:32 +0500

    Add model of depot type table

---
+++ src/main/java/com/muv/railroadrepair/entity/DepotType.java
@@ -0,0 +1,23 @@
+package com.muv.railroadrepair.entity;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Entity
+@Table(name = "depot_type", schema = "public", catalog = "railroad_transport")
+public class DepotType {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id", nullable = false)
+    private int id;
+    @Basic
+    @Column(name = "type", nullable = false, length = 100)
+    private String type;
+}
