a5676b35c3376f58a1b5b88f95ceb5bc8e17bb9b
muv11 <uvmozgovaya@proton.me>
2023-07-31 11:48:40 +0500

    Add ProjectConfiguration, create datasource and entity manager beans

---
+++ src/main/java/com/muv/railroadrepair/configuration/ProjectConfiguration.java
@@ -0,0 +1,56 @@
+package com.muv.railroadrepair.configuration;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+
+@Configuration
+@ComponentScan(basePackages = "java")
+@PropertySource("classpath:application.properties")
+public class ProjectConfiguration {
+
+    private final Environment environment;
+
+    public ProjectConfiguration(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource managerDataSource = new DriverManagerDataSource();
+        managerDataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("spring.datasource.driver-class-name")));
+        managerDataSource.setUrl(Objects.requireNonNull(environment.getProperty("spring.datasource.url")));
+        managerDataSource.setUsername(Objects.requireNonNull(environment.getProperty("spring.datasource.username")));
+        managerDataSource.setPassword(Objects.requireNonNull(environment.getProperty("spring.datasource.password")));
+        return managerDataSource;
+    }
+
+    @Bean()
+    public EntityManagerFactory entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setPersistenceUnitName("PERSISTENCE_UNIT");
+        em.setDataSource(dataSource());
+        em.setPackagesToScan("com.muv");
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        em.afterPropertiesSet();
+        return em.getObject();
+    }
+
+    @Bean
+    public EntityManager entityManager() {
+        try (EntityManagerFactory entityManagerFactory = entityManagerFactory()) {
+            return entityManagerFactory.createEntityManager();
+        }
+    }
+
+}
