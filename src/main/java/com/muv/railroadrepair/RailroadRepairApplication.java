c977a5dcfab66a08809e040a85063af1ed9ffdfc
muv11 <uvmozgovaya@proton.me>
2023-07-31 11:39:57 +0500

    Initial commit

---
+++ src/main/java/com/muv/railroadrepair/RailroadRepairApplication.java
@@ -0,0 +1,13 @@
+package com.muv.railroadrepair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailroadRepairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailroadRepairApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muv/railroadrepair/RailroadRepairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muv.railroadrepair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailroadRepairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
