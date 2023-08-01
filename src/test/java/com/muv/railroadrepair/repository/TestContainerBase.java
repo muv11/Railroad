58ae67691b67c808f7dee1f1288531d767d2edde
muv11 <uvmozgovaya@proton.me>
2023-08-01 17:37:12 +0500

    Create class that has postgresql container with testcontainers library

---
+++ src/test/java/com/muv/railroadrepair/repository/TestContainerBase.java
@@ -0,0 +1,38 @@
+package com.muv.railroadrepair.repository;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class TestContainerBase {
+
+    static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER;
+
+    static {
+        POSTGRE_SQL_CONTAINER = new PostgreSQLContainer<>("postgres:15")
+                .withDatabaseName("geography_test")
+                .withUsername("test")
+                .withPassword("1234")
+                .withInitScript("sql/region.sql");
+    }
+
+    @DynamicPropertySource
+    static void databaseProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRE_SQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRE_SQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRE_SQL_CONTAINER::getPassword);
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        POSTGRE_SQL_CONTAINER.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        POSTGRE_SQL_CONTAINER.stop();
+    }
+
+}
