694321b24272fa002e9b378120bfc82b25269a1b
muv11 <uvmozgovaya@proton.me>
2023-08-04 15:26:35 +0500

    Create controller for login with get mapping method

---
+++ src/main/java/com/muv/railroadrepair/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.muv.railroadrepair.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/")
+    public String getLogin() {
+        return "index";
+    }
+
+}
