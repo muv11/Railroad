5a7d3c90983c6ab5bd7b3c1cda918d2a8df7bf05
muv11 <uvmozgovaya@proton.me>
2023-08-05 18:28:51 +0500

    Create repository for city table with findAllByRegionName method

---
+++ src/main/java/com/muv/railroadrepair/repository/CityRepository.java
@@ -0,0 +1,23 @@
+package com.muv.railroadrepair.repository;
+
+import com.muv.railroadrepair.entity.City;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CityRepository {
+
+    private final EntityManager entityManager;
+
+    public CityRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public List<City> findAllByRegionName(String name) {
+        return entityManager.createQuery("SELECT city FROM City city WHERE name = :name", City.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
